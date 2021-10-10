d59ba097c2d1f5978a183b6ed98edbff2bd29108
HarshitaSingh47 <harshitasingh47@gmail.com>
2021-10-10 14:28:35 -0500

    working sys calls

--- wish.c
+++ wish.c
@@ -4,15 +4,37 @@
 #include<ctype.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
 
 void runCD(char** cdargs){};
+char path[] = {"/bin"};
 
+//change method
+char **get_input(char *input) {
+    char **command = malloc(8 * sizeof(char *));
+    char *separator = " ";
+    char *parsed;
+    int index = 0;
+
+    parsed = strtok(input, separator);
+    while (parsed != NULL) {
+        command[index] = parsed;
+        index++;
+
+        parsed = strtok(NULL, separator);
+    }
+
+    command[index] = NULL;
+    return command;
+}
 
 void runLS(){
      int status;
     char *args[2];
 
-    args[0] = "/bin/ls";        
+    args[0] = "/bin";        
     args[1] = NULL;           
 
     if ( fork() == 0 )
@@ -20,6 +42,7 @@ void runLS(){
     else
         wait( &status );        
 }
+
 
 void runLSwithArgs(char** lsargs){
     int status;
@@ -38,7 +61,8 @@ void runLSwithArgs(char** lsargs){
         wait( &status );        
 }
 
-void sanitizeInput(char* input){
+void sanitizeInput(char* input){\
+    printf("Sanitizing input\n");
     char *token;
     char *pointer;
     pointer = input;
@@ -57,12 +81,31 @@ void sanitizeInput(char* input){
         i++;
     } while ((token = strtok(NULL, " \t\n")) != NULL);
 
-    if(strcmp(dict[0],"ls")==0 && (dict[1] || dict[2] )) { runLSwithArgs(dict); return; }
-    else if(strcmp(dict[0],"ls")==0 ) { runLS(); return; }
-    if(strcmp(dict[0],"cd")==0){
+    //if(strcmp(dict[0],"ls")==0 && (dict[1] || dict[2] )) { runLSwithArgs(dict); return; }
+    
+    if(strcmp(dict[0],"cd" ) == 0  ){
+        printf("We got a cd\n");
+        int rc = fork();
+        int stat;
+        if (rc < 0) {
+            fprintf(stderr, "fork failed\n");
+            exit(1);
+        } else if (rc == 0) {
+            printf("try to chdir\n");
+            int ret = chdir(dict[1]);
+            printf("%d\n",ret);
+            
+
         
+        }
+        else {
+        int rc_wait = wait(&stat);
+        char s[100];
+        printf("%s\n", getcwd(s, 100));
+        }
     }
 
+    else { runLS(); return; }
 
 
 
@@ -120,26 +163,54 @@ int main(int argc, char* argv[]){
     }
     else{
 
-        buffer = (char *)malloc(bufsize * sizeof(char));
-        if( buffer == NULL)
-        {
-            perror("Unable to allocate buffer");
-            exit(1);
+        // buffer = (char *)malloc(bufsize * sizeof(char));
+        // if( buffer == NULL)
+        // {
+        //     perror("Unable to allocate buffer");
+        //     exit(1);
+        // }
+
+        // while(1){
+        //     printf(">wish ");
+        //     getline(&buffer,&bufsize,stdin);
+        //     //printf("%s",buffer);
+        //     parseInput(buffer);
+        //     buffer[strcspn(buffer, "\n")] = 0;
+        //     if(strcmp(buffer,"exit")==0){
+        //         exit(0);
+        //     }
+
+
+   char **command;
+    char *input;
+    pid_t child_pid;
+    int stat_loc;
+
+    while (1) {
+        input = readline("unixsh> ");
+        command = get_input(input);
+
+        if (!command[0]) {      /* Handle empty commands */
+            free(input);
+            free(command);
+            continue;
         }
 
-        while(1){
-            printf(">wish ");
-            getline(&buffer,&bufsize,stdin);
-            //printf("%s",buffer);
-            parseInput(buffer);
-            buffer[strcspn(buffer, "\n")] = 0;
-            if(strcmp(buffer,"exit")==0){
-                exit(0);
-            }
-            
+        child_pid = fork();
+        if (child_pid == 0) {
+            /* Never returns if the call is successful */
+            execvp(command[0], command);
+            printf("This won't be printed if execvp is successul\n");
+        } else {
+            waitpid(child_pid, &stat_loc, WUNTRACED);
         }
+
+        free(input);
+        free(command);
     }
 
+ 
+    }
 return 0;
 }
 
