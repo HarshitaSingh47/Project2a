fe1d9c740b4808f6528455c85355cc6af7c4fa27
HarshitaSingh47 <harshitasingh47@gmail.com>
2021-10-11 20:31:14 -0500

    test 1-5 passed

--- wish-test.c
+++ wish-test.c
@@ -8,14 +8,112 @@
 
 char **get_input(char *);
 int index_cmd;
-
+int EXITSHELL = 0;
+int wish_cd(char**);
+int wish_loop(char**);
+int wish_path(char**);
+int wish_exit(char**);
 int index_path = 0 ; 
+char *builtin_cmd[] = {"cd",  "exit"}; //"loop", "path",
+
+int (*builtin_func[]) (char **) = {&wish_cd , &wish_exit}; //&wish_loop,&wish_path,  &wish_exit
 
 
+int wish_exit(char** command){
+
+    if(command[1]!=NULL){
+        char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
+    }
+    else{
+    // EXITSHELL = 1;
+    exit(0);
+    }
+	return 0;
 
 
+}
 
-int main() {
+int wish_cd(char** command){
+
+    if (command[1] == NULL) 
+	{
+		char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
+	} 
+	else 
+	{
+		if (chdir(command[1]) != 0) 
+		{
+			char error_message[30] = "An error has occurred\n";
+            write(STDERR_FILENO, error_message, strlen(error_message));
+		}
+	}
+	return 1;
+}
+
+int execSystemCommands(char** command){
+    pid_t child_pid;
+    int stat_loc;
+    int ret = 0;
+    child_pid = fork();
+    if (child_pid == 0) {
+            
+        ret = execvp(command[0], command);
+        printf("This won't be printed if execvp is successul\n");
+    } else {
+        waitpid(child_pid, &stat_loc, WUNTRACED);
+    }
+    return ret;
+
+}
+
+int execCommands(char** command){
+    int ret = 0;
+    int num = sizeof(builtin_cmd)/sizeof(char *);
+    if(command[0]==NULL){
+        return 1; 
+    }
+    for(int i; i< num ; i++){
+        if(strcmp(command[0],builtin_cmd[i])==0){
+            //printf("Checking in exec commands\n");
+            return (*builtin_func[i])(command);
+        }
+    }
+
+    ret = execSystemCommands(command);
+
+    return ret;
+}
+
+int runbatchmode(char filename[100]){
+    FILE *fptr;
+	char line[200];
+	char **args;
+	fptr = fopen(filename, "r");
+	if (fptr == NULL)
+	{
+		printf("\nUnable to open file.");
+		return 1;
+	}
+	else
+	{
+		//printf("\nFile Opened. Parsing. Parsed commands displayed first.");
+		while(fgets(line, sizeof(line), fptr)!= NULL)
+		{
+			//printf("\n%s", line);
+			args=get_input(line);
+           // printf("Returned from get input with value in c[0] as %s\n", args[0]);
+			execCommands(args);
+		}
+	}
+	free(args);
+	fclose(fptr);
+	return 1;
+}
+
+
+int main(int argc, char **argv) {
     char **command;
     pid_t child_pid;
     int stat_loc;
@@ -24,9 +122,15 @@ int main() {
     char **path = calloc(32, sizeof(char *));
     int i;
     
-            
+        if(argc==2){
 
-    while (1) {
+            runbatchmode(argv[1]);
+            
+        
+        }
+        else if(argc==1){
+
+         while (1) {
 
         buffer = (char *)malloc(bufsize * sizeof(char));
         if( buffer == NULL)
@@ -42,7 +146,6 @@ int main() {
 
         command = get_input(buffer);
        // printf("This is the size: %d\n",index_cmd);
-        
 
         if (!command[0]) {      /* Handle empty commands */
             free(buffer);
@@ -279,25 +382,56 @@ int main() {
         free(buffer);
         free(command);
     }
+    }
 
     return 0;
 }
 
 char **get_input(char *input) {
-    char **command = malloc(8 * sizeof(char *));
-    char *separator = " >";
-    char *parsed;
-    index_cmd = 0;
 
-    parsed = strtok(input, separator);
-    while (parsed != NULL) {
+    //printf("We are in get input\n");
+    char **tokens = (char **)malloc(sizeof(char *) * 64);
+	char *token;
+	char delim[10] = " \t\n\r\a>";
+	int index_cmd = 0, bufsize = 64;
+	if (!tokens)
+	{
+		printf("\nBuffer Allocation Error.");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(input, delim);
+	while (token != NULL)
+	{
+		tokens[index_cmd] = token;
+		index_cmd ++;
+		if (index_cmd >= bufsize)
+		{
+			bufsize += 64;
+			input = realloc(input, bufsize * sizeof(char *));
+			if (!input) // Buffer Allocation Failed
+			{
+			printf("\nBuffer Allocation Error.");
+			exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, delim);
+	}
+	tokens[index_cmd] = NULL;
+	return tokens;
+    // char **command = malloc(8 * sizeof(char *));
+    // char *separator = " >\n";
+    // char *parsed;
+    // index_cmd = 0;
+    // printf("We are in get input\n");
+    // parsed = strtok(input, separator);
+    // while (parsed != NULL) {
         
-        command[index_cmd] = parsed;
-        index_cmd++;
+    //     command[index_cmd] = parsed;
+    //     index_cmd++;
 
-        parsed = strtok(NULL, separator);
-    }
+    //     parsed = strtok(NULL, separator);
+    // }
 
-    command[index_cmd] = NULL;
-    return command;
+    // command[index_cmd] = NULL;
+    // return command;
 }
